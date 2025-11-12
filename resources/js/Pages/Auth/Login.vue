a72dba1ecaec2c3bb19c94475b4deac9a32a3ce7
zilef2 <ajelof2@gmail.com>
2025-11-12 12:29:42 -0500

    test no solo t

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -62,12 +62,12 @@ const submit = () => {
 <!--                </label>-->
 <!--            </div>-->
             <div class="flex items-center justify-between mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary dark:focus:ring-offset-gray-800">
-                {{ lang().label.lost_password }}
-                </Link>
+<!--                <Link v-if="canResetPassword" :href="route('password.request')"-->
+<!--                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary dark:focus:ring-offset-gray-800">-->
+<!--                {{ lang().label.lost_password }}-->
+<!--                </Link>-->
                 <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    {{ form.processing ? lang().button.login + '...' : lang().button.login }} 
+                    {{ form.processing ? '...' : lang().button.login }} 
                 </PrimaryButton>
             </div>
         </form>
