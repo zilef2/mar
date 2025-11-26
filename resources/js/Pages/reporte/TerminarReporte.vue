2ea5e29e8a15f967dc1d51c176ec0024dc7f14cb
zilef2 <ajelof2@gmail.com>
2025-11-26 12:44:47 -0500

    no expired2

--- resources/js/Pages/reporte/TerminarReporte.vue
+++ resources/js/Pages/reporte/TerminarReporte.vue
@@ -5,11 +5,12 @@ import Modal from '@/Components/Modal.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { useForm } from '@inertiajs/vue3';
-import { watchEffect, reactive } from 'vue';
-import vSelect from "vue-select"; import "vue-select/dist/vue-select.css";
+import {useForm} from '@inertiajs/vue3';
+import {watchEffect, reactive} from 'vue';
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 
-import { TimeTo12Format, formatTime, formatDate, TransformTdate } from '@/global.ts';
+import {TimeTo12Format, formatTime, formatDate, TransformTdate} from '@/global.ts';
 
 const props = defineProps({
     show: Boolean,
@@ -25,10 +26,10 @@ const data = reactive({
     ],
 })
 
-const form = useForm({ ...Object.fromEntries(data.justNames.map(field => [field, ''])) });
+const form = useForm({...Object.fromEntries(data.justNames.map(field => [field, '']))});
 const printForm = [
     // { idd: 'codigo', label: 'codigo', type: 'text', value: form.codigo , elif:null},
-    { idd: 'hora_final', label: 'hora final', type: 'time', value: form.hora_final , elif:null},
+    {idd: 'hora_final', label: 'hora final', type: 'time', value: form.hora_final, elif: null},
 ];
 
 watchEffect(() => {
@@ -40,7 +41,7 @@ watchEffect(() => {
         //# hora final como la hora del sistema
         const validDate = new Date()
         let hora = validDate.getHours();
-        let hourAndtime =  (hora < 10 ? '0': '')+ (hora) + ':'+ (validDate.getMinutes() < 10 ? '0': '') + validDate.getMinutes() + ':00';
+        let hourAndtime = (hora < 10 ? '0' : '') + (hora) + ':' + (validDate.getMinutes() < 10 ? '0' : '') + validDate.getMinutes() + ':00';
         form.hora_final = hourAndtime
 
 
@@ -70,27 +71,40 @@ const update = () => {
                 </h2>
                 <div class="grid grid-cols-1 md:grid-cols-1 md:mx-[100px] mt-8 gap-6">
 
-                    <div v-for="(atributosform, indice) in printForm" :key="indice">
-
-                        <div v-if="atributosform.type === 'time'" id="SelectVue">
-                            <InputLabel class="text-center mr-8"
-                                :for="atributosform.label" :value="lang().label[atributosform.label]" />
-                            <TextInput
-                                :id="atributosform.idd" :type="atributosform.type" class="px-auto w-full text-xl text-center"
-                                v-model="form[atributosform.idd]" required :placeholder="atributosform.label"
-                                :error="form.errors[atributosform.idd]" disabled
-                                step="3600"
+                    <div v-for="(campo, i) in printForm" :key="i">
+                        <div v-if="campo.type === 'time'" class="inline-flex w-full items-center space-x-6">
+                            <InputLabel
+                                :for="campo.label"
+                                :value="lang().label[campo.label]"
+                                class="text-center w-40"
                             />
-                            <InputError class="mt-2" :message="form.errors[atributosform.idd]" />
+
+                            <div class="flex-1">
+                                <TextInput
+                                    :id="campo.idd"
+                                    :type="campo.type"
+                                    class="w-full text-xl text-center"
+                                    v-model="form[campo.idd]"
+                                    :placeholder="campo.label"
+                                    :error="form.errors[campo.idd]"
+                                    required
+                                    disabled
+                                    step="3600"
+                                />
+                                <InputError
+                                    :message="form.errors[campo.idd]"
+                                    class="mt-1"
+                                />
+                            </div>
                         </div>
                         <div v-else class="">
                             <InputLabel
-                                :for="atributosform.label" :value="lang().label[atributosform.label]" />
+                                :for="campo.label" :value="lang().label[campo.label]"/>
                             <TextInput
-                                 :id="atributosform.idd" :type="atributosform.type" class="mt-1 block w-full"
-                                v-model="form[atributosform.idd]" :placeholder="atributosform.label"
-                                :error="form.errors[atributosform.idd]" />
-                                <InputError class="mt-2" :message="form.errors[atributosform.idd]" />
+                                :id="campo.idd" :type="campo.type" class="mt-1 block w-full"
+                                v-model="form[campo.idd]" :placeholder="campo.label"
+                                :error="form.errors[campo.idd]"/>
+                            <InputError class="mt-2" :message="form.errors[campo.idd]"/>
                         </div>
                     </div>
 
@@ -99,7 +113,7 @@ const update = () => {
                     <SecondaryButton :disabled="form.processing" @click="emit('close')"> Cancelar
                     </SecondaryButton>
                     <PrimaryButton class="ml-3" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
-                        @click="update">
+                                   @click="update">
                         {{ form.processing ? 'Cerrar Tarea' + '...' : 'Cerrar Tarea' }}
                     </PrimaryButton>
                 </div>
